a0ac0eaab8db5b6a49117f0be00a0147b6ef2e2e
Aru2003 <baymolda03@inbox.ru>
2023-10-22 21:51:36 +0600

    Добавьте файлы проекта.

---
+++ modul8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class SquaredArray
+{
+    private double[] data; // Одномерный массив данных
+
+    public SquaredArray(int size)
+    {
+        data = new double[size];
+    }
+
+    // Индексатор для массива
+    public double this[int index]
+    {
+        get
+        {
+            return data[index];
+        }
+        set
+        {
+            data[index] = value * value; // Возводим значение в квадрат перед сохранением
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int size = 5;
+        SquaredArray arr = new SquaredArray(size);
+
+        // Установка значений с автоматическим возведением в квадрат
+        arr[0] = 2;
+        arr[1] = 3;
+        arr[2] = 4;
+        arr[3] = 5;
+        arr[4] = 6;
+
+        // Получение и вывод элементов массива
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"arr[{i}] = {arr[i]}");
+        }
+    }
+}
